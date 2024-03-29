dfc454a1f0d19a2fef52cc20ec0f8cf8f8e2496b
helaoui slim <s.helaoui-ext@matawan-mobility.com>
2024-03-29 10:23:15 +0100

    Initial commit

---
+++ app/src/main/java/com/example/training_ci_cd/MainActivity.kt
@@ -0,0 +1,43 @@
+package com.example.training_ci_cd
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.training_ci_cd.ui.theme.Training_ci_cdTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            Training_ci_cdTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    Training_ci_cdTheme {
+        Greeting("Android")
+    }
+}
